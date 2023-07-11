1a209784592bcd5394672d06dc24fc24f2ffc67e
thuyencode <thuyencode@proton.me>
2023-07-11 14:50:12 +0700

    ⚡️ Speed up dev server and support older browsers using new plugins

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
   base: '/pomodoro-clock/'
 })
