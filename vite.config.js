0b3589681a89662296a8d4e542b94498f308cf8f
thuyencode <thuyencode@proton.me>
2023-07-11 16:07:43 +0700

    ⚡️ Million JS go brrrrr

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
+import million from 'million/compiler'
 import react from '@vitejs/plugin-react-swc'
 import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
+// https://million.dev/docs/install#use-the-compiler
 export default defineConfig({
   plugins: [
+    million.vite(),
     react(),
     legacy({
       targets: ['defaults', 'not IE 11']
